9dd6952d11f7eff4de15db433f15e005e12285b3
VictorBK <victorbrian4@gmail.com>
2022-05-30 23:31:35 +0300

    this is a technical interview preparation kind of task whereby one needs to write a function in C that checks if a singly linked list has a cycle in it

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,25 +1,31 @@
+#include <stdio.h>
 #include "lists.h"
+
 /**
- * check_cycle - checks if singly-linked list is a cycle
- * @list: head of linked list
- * Return: 1 if there is a cycle, 0 if there is no cycle
- */
+* check_cycle - checks if a singly linked list has a cycle within.
+* @list: singly linked list.
+* Return: 0 is there is no cycle, 1 if there is a cycle.
+*/
+
 int check_cycle(listint_t *list)
 {
-	listint_t *fast_pointer, *slow_pointer;
+	listint_t *head;
+	listint_t *tail;
 
 	if (list == NULL)
 		return (0);
 
-	fast_pointer = list;
-	slow_pointer = list;
-	while (fast_pointer && slow_pointer->next)
+	head = list;
+	tail = list;
+
+	while (tail != NULL && tail->next != NULL)
 	{
-		list = list->next;
-		slow_pointer = slow_pointer->next;
-		fast_pointer = fast_pointer->next->next;
-		if (fast_pointer == slow_pointer)
+		head = head->next;
+		tail = tail->next->next;
+
+		if (head == tail)
 			return (1);
+
 	}
 	return (0);
 }
