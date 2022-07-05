e38595095f84c3b4195450e4debef784ccc70a6a
VictorBK <victorbrian4@gmail.com>
2022-07-05 15:53:03 +0300

    Read file mended

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,6 +3,7 @@
 Incorporates a function that reads and prints contents from file
 """
 
+
 def read_file(filename=""):
     """ Read and print text from file """
     with open(filename, encoding="utf-8") as fd:
