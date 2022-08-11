5bb9a8aceb4a6d29d190f03cbd2f8369c21ec1f6
VictorBK <victorbrian4@gmail.com>
2022-08-11 20:27:18 +0300

    Temperatures #1

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,3 +1,4 @@
-- displays the top 3 of cities temperature during July and August ordered by temperature (descending)
+-- Import in hbtn_0c_0 database this table dump: sql file
+-- script that displays the average temperature (Fahrenheit) by city ordered by temperature (descending)
 
-SELECT city, AVG(value) as 'avg_temp' FROM temperatures WHERE `month`=7 OR `month`=8 GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
+SELECT city, avg(value) as avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp desc;
